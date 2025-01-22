bd7fa3aabe17c3cf869e1d458b4f52973eb4763e
unknown <yaroslavakray@gmail.com>
2025-01-23 00:42:05 +0200

    Homework7

---
+++ Homework7/task1.js
@@ -0,0 +1,13 @@
+function handleNum(number, callbackParne, callbackNeParne) {
+  
+    if (number % 2 === 0) {
+      callbackParne(number);
+    } else {
+      callbackNeParne(number);
+    }
+  }
+  const handleEven = (num) => console.log(`${num} - number is even`);
+  const handleOdd = (num) => console.log(`${num} - number is odd`);
+  
+  handleNum(6, handleEven, handleOdd);
+  handleNum(3, handleEven, handleOdd); 
